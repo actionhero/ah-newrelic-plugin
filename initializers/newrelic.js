b6195fb03ac3bfa7689ccf910a93f591118e084b
Evan Tahler <evan@taskrabbit.com>
2015-01-14 21:09:17 -0800

    update for actionhero v10

--- initializers/newrelic.js
+++ initializers/newrelic.js
@@ -1,38 +1,29 @@
-exports.newrelic = function(api, next){
+var newrelic = require("newrelic");
 
-  api.newrelic = {};
+module.exports = {
+  initialize: function(api, next){
+    api.newrelic = {
+      middleware: function(connection, actionTemplate, next){
+        if(connection.type === 'web'){
+          // for now, the node newrelic agent only supports HTTP requests
+          newrelic.setTransactionName(actionTemplate.name);
+        }
+        next(connection, true);
+      },
 
-  if(api.env === 'production'){
-
-    newrelic = require("newrelic");
-
-    api.newrelic.middleware = function(connection, actionTemplate, next){
-      if(connection.type === 'web'){
-        // for now, the node newrelic agent only supports HTTP requests
-        newrelic.setTransactionName(actionTemplate.name);
+      errorReporter: function(type, err, extraMessages, severity){
+        newrelic.noticeError(err);
       }
-      next(connection, true);
-    }
-
-    api.newrelic.errorReporter = function(type, err, extraMessages, severity){
-      newrelic.noticeError(err);
-    }
-
-    api.newrelic._start = function(api, next){
-      // load the newrelic middleware into actionhero
-      api.actions.addPreProcessor( api.newrelic.middleware );
-      // load the newrelic error reporter into actionhero
-      api.exceptionHandlers.reporters.push( api.newrelic.errorReporter );
-      // optional: ignore certain actions
-      // newrelic.setIgnoreTransaction('actionName');
-      next();
     };
+  },
 
-    api.newrelic._stop =  function(api, next){
-      next();
-    };
-
+  start: function(api, next){
+    // load the newrelic middleware into actionhero
+    api.actions.addPreProcessor( api.newrelic.middleware );
+    // load the newrelic error reporter into actionhero
+    api.exceptionHandlers.reporters.push( api.newrelic.errorReporter );
+    // optional: ignore certain actions
+    // newrelic.setIgnoreTransaction('actionName');
+    next();
   }
-
-  next();
-}
+};
